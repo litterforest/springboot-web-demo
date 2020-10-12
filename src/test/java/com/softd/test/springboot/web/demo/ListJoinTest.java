87f71f082ac34835766ed727f6d84108e9214a20
cobee <18027041863@163.com>
2020-10-12 23:13:23 +0800

    优化代码

--- src/test/java/com/softd/test/springboot/web/demo/ListJoinTest.java
+++ src/test/java/com/softd/test/springboot/web/demo/ListJoinTest.java
@@ -16,9 +16,9 @@ import java.util.stream.Collectors;
 public class ListJoinTest {
     public static void main(String[] args) {
         List<User> userList = new ArrayList<>();
-        userList.add(new User("张三"));
-        userList.add(new User(null));
-        userList.add(new User("王五"));
+//        userList.add(new User("张三"));
+//        userList.add(new User(null));
+//        userList.add(new User("王五"));
         String result = userList.stream().map(user -> {
             if (StringUtils.isNotBlank(user.getName())) {
                 return user.getName();
